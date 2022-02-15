ee66d9ba39108de1aa4c1b558a8634a543570d3f
Liam Salomonsson <liam.salomonsson@gmail.com>
2022-02-15 09:47:40 +0100

    working on defining functions

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
+const _ = require("lodash");
+
 var employerContribution = 31.42;
 var wellnessAllowance = 5000;
 var delayInMonthsForInvoiceToBePaid = 3;
 var incomeBaseAmount = 71000;
+var incomeTaxPercentage = 30;
+var employeOfficeSuppliesPerYear = 30000;
 
 class Employee {
-    constructor(age, startDate, endDate, rate,) {
+    constructor(age,
+        startDate,
+        endDate,
+        rate,
+        expectedDebitedPercentage,
+        bonusPercentages,
+        recruitmentCost,
+        salary) {
+        this.salary = salary;
+        this.recruitmentCost = recruitmentCost;
+        this.bonusPercentages = bonusPercentages;
+        this.expectedDebitedPercentage = expectedDebitedPercentage;
         this.rate = rate;
         this.age = age;
         this.startDate = startDate;
@@ -12,8 +27,16 @@ class Employee {
     }
 }
 
-var calculatePensionCost = (salary) => {
-    // 30% eligable = salary*12 - (incombaseAmount*7,5) / 12 
+var calculatePensionCostPerMonth = (salary) => {
+    var monthlySalaryBreakpoint = (incomeBaseAmount * 7.5) / 12;
+
+    var pension = 0;
+    if (salary > monthlySalaryBreakpoint) {
+        pension = (salary - monthlySalaryBreakpoint) * 0.3
+    }
+
+    pension += salary * 0.045;
+    return pension;
 }
 
 var calculateMonthlyIncome = (debitedHours, rate) => {
@@ -25,23 +48,73 @@ var calculateMonthlyIncome = (debitedHours, rate) => {
 
 var calculateDebitedHours = (startDate, endDate, employee) => {
     // for each day
-        // if its a working day && employee is not sick && employee is not on holiday && employee is not in education
-            // add 8h
-        
+    // if its a working day && employee is not sick && employee is not on holiday && employee is not in education
+    // add 8h
+
 }
 
-var montlyMaximumDebitableHours = (startYear, endYear) => {
-    // for each year
-        // for each month
-            // if is working day add 8
+var getDatesBetween = (startDate, endDate) => {
+    let dates = [];
+    let loop = new Date(startDate);
+    while (loop <= endDate) {
+        dates.push(loop);
 
+        let newDate = loop.setDate(loop.getDate() + 1);
+        loop = new Date(newDate);
+    }
+
+    return dates;
+}
+
+var montlyMaximumDebitableHours = (startDate, endDate) => {
+    let loop = new Date(startDate);
+    let debitableHoursPerDay = [];
+
+    while (loop <= endDate) {
+        var dayOfWeek = loop.getDay();
+        var isWeekend = (dayOfWeek === 5) || (dayOfWeek === 6);
+
+        debitableHoursPerDay.push({
+            Day: loop,
+            Hours: isWeekend ? 0 : 8,
+            Month: loop.getMonth() + 1
+        });
+
+        let newDate = loop.setDate(loop.getDate() + 1);
+        loop = new Date(newDate);
+    }
+
+    var groupedByMonth = _(debitableHoursPerDay)
+        .groupBy("Month")
+        .map((item, Month) => ({
+            month: Month,
+            totalHours: _.sumBy(item, "Hours")
+        }))
+        .value();
+
+    return groupedByMonth;
 };
 
-var employee1 = new Employee(12, 
-    new Date("2022-01-01"), 
+var employee1 = new Employee(12,
+    new Date("2022-01-01",),
     new Date("2022-10-01"),
     800,
+    90,
+    12,
+    0,
+    60000
+);
 
-    );
+var pension = calculatePensionCostPerMonth(employee1.salary);
 
-console.log(employee1.age + " " + employee1.startDate + " " + employee1.endDate);
+let monthlyMaximumDebitableHours = montlyMaximumDebitableHours(new Date(2022, 0, 1), new Date(2022, 11, 31));
+
+let defaultHolidays = () => {
+
+    let dates = getDatesBetween(new Date(2022, 5, 1), new Date(2022, 6, 16));
+    dates.push(...getDatesBetween(new Date(2022, 11, 15), new Date(2022, 11, 31)));
+
+    return dates;
+};
+
+console.log(defaultHolidays());
