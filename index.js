13e82bfff92983db627f88829a988ecd43223751
Liam Salomonsson <liam.salomonsson@telenor.se>
2022-02-18 13:16:09 +0100

    more work on functions

--- index.js
+++ index.js
@@ -41,16 +41,14 @@ var calculatePensionCostPerMonth = (salary) => {
 
 var calculateMonthlyIncome = (debitedHours, rate) => {
     // for each month get debited hours
-    debitedHours * rate
-
-    // return income for each month
+    return debitedHours * rate
 };
 
 var calculateDebitedHours = (startDate, endDate, employee) => {
     // for each day
     // if its a working day && employee is not sick && employee is not on holiday && employee is not in education
     // add 8h
-
+    
 }
 
 var getDatesBetween = (startDate, endDate) => {
@@ -66,26 +64,28 @@ var getDatesBetween = (startDate, endDate) => {
     return dates;
 }
 
-var montlyMaximumDebitableHours = (startDate, endDate) => {
-    let loop = new Date(startDate);
+var debitableHoursPerDay = (startDate, endDate) => {
     let debitableHoursPerDay = [];
+    var dates = getDatesBetween(startDate, endDate);
 
-    while (loop <= endDate) {
-        var dayOfWeek = loop.getDay();
+    dates.forEach(date => {
+        var dayOfWeek = date.getDay();
         var isWeekend = (dayOfWeek === 5) || (dayOfWeek === 6);
 
         debitableHoursPerDay.push({
-            Day: loop,
+            Day: date,
             Hours: isWeekend ? 0 : 8,
-            Month: loop.getMonth() + 1
+            Month: date.getMonth() + 1,
+            Year: date.getYear()
         });
+    });
 
-        let newDate = loop.setDate(loop.getDate() + 1);
-        loop = new Date(newDate);
-    }
+    return debitableHoursPerDay;
+};
 
+var maximumDebitableHoursGrouped = (debitableHoursPerDay, groupedBy) => {
     var groupedByMonth = _(debitableHoursPerDay)
-        .groupBy("Month")
+        .groupBy(groupedBy)
         .map((item, Month) => ({
             month: Month,
             totalHours: _.sumBy(item, "Hours")
@@ -93,28 +93,67 @@ var montlyMaximumDebitableHours = (startDate, endDate) => {
         .value();
 
     return groupedByMonth;
-};
+}
 
-var employee1 = new Employee(12,
-    new Date("2022-01-01",),
-    new Date("2022-10-01"),
-    800,
-    90,
-    12,
-    0,
-    60000
-);
-
-var pension = calculatePensionCostPerMonth(employee1.salary);
-
-let monthlyMaximumDebitableHours = montlyMaximumDebitableHours(new Date(2022, 0, 1), new Date(2022, 11, 31));
+var pension = calculatePensionCostPerMonth(60000);
 
 let defaultHolidays = () => {
-
-    let dates = getDatesBetween(new Date(2022, 5, 1), new Date(2022, 6, 16));
-    dates.push(...getDatesBetween(new Date(2022, 11, 15), new Date(2022, 11, 31)));
-
-    return dates;
+    let defaultHolidays = [];
+    defaultHolidays.push(...getDatesBetween(new Date(2022, 5, 1), new Date(2022, 6, 16)));
+    defaultHolidays.push(...getDatesBetween(new Date(2022, 11, 15), new Date(2022, 11, 31)));
+    return defaultHolidays;
 };
 
-console.log(defaultHolidays());
+let defaultSickness = () => {
+    let defaultSickness = [];
+    defaultSickness.push(...getDatesBetween(new Date(2022, 2, 1), new Date(2022, 2, 6)));
+    defaultSickness.push(...getDatesBetween(new Date(2022, 9, 15), new Date(2022, 9, 22)));
+    return defaultSickness;
+}
+
+let defaultEducation = () => {
+    let defaultEducation = [];
+    defaultEducation.push(...getDatesBetween(new Date(2022, 1, 15), new Date(2022, 1, 17)));
+    defaultEducation.push(...getDatesBetween(new Date(2022, 3, 15), new Date(2022, 3, 16)));
+    return defaultEducation;
+}
+
+var dhpd = debitableHoursPerDay(new Date(2022, 0, 1), new Date(2022, 11, 31));
+
+let containsDate = (arrayOfDates, item) => {
+    return arrayOfDates.some(date => 
+        date.getDate() === item.getDate() 
+        && date.getMonth() === item.getMonth()
+        && date.getYear() === item.getYear()) 
+};
+
+var debitableHoursPerDayWithRemovedHolidays = dhpd.filter((item) => {
+    let nonDebitableDays = defaultHolidays().concat(defaultSickness(), defaultEducation());
+    
+    if(containsDate(nonDebitableDays, item.Day)){
+        return false;
+    }else{
+        return true;
+    }
+})
+
+var maximumDebitableHoursPerMonth = maximumDebitableHoursGrouped(debitableHoursPerDayWithRemovedHolidays, "Month");
+var yearly = maximumDebitableHoursGrouped(debitableHoursPerDayWithRemovedHolidays, "Year");
+let employee = new Employee(25, new Date(2022, 1, 1), new Date(2025, 1, 1), 800, 90, [6,12], 0, 55000); 
+
+// // income from employee
+// console.log("////////////////// Income ///////////////////");
+// maximumDebitableHoursPerMonth.forEach((item) =>{
+//     console.log("Monthly income: " + calculateMonthlyIncome(item.totalHours, employee.salary));
+// });
+// console.log("////////////////////////////////////////////");
+
+// console.log("");
+
+// // costs for employee
+// console.log("////////////////// Costs ///////////////////");
+// console.log(calculatePensionCostPerMonth(employee.salary))
+// console.log("////////////////////////////////////////////");
+
+
+// how to calculate 
