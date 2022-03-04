ca0bc9e6f956ecc7f2e7afb7ae5c73f18c332f86
Liam Salomonsson <liam.salomonsson@telenor.se>
2022-03-04 15:51:11 +0100

    created a nextjs web and started moving calculations

--- shiftkey-offer/calculations/calculations.ts
+++ shiftkey-offer/calculations/calculations.ts
@@ -1,6 +1,6 @@
-const _ = require("lodash");
+import _ from "lodash";
 
-var calculatePensionCostPerMonth = (salary) => {
+const calculatePensionCostPerMonth = (salary: number) => {
     const incomeBaseAmount = 71000;
     var monthlySalaryBreakpoint = (incomeBaseAmount * 7.5) / 12;
 
@@ -10,14 +10,14 @@ var calculatePensionCostPerMonth = (salary) => {
     }
 
     pension += salary * 0.045;
-    return pension;
+    return Math.floor(pension/100)*100;
 }
 
-var getTotalIncome = (debitedHours, rate) => {
+const getTotalIncome = (debitedHours: number, rate: number) => {
     return debitedHours * rate;
 };
 
-var getDatesBetween = (startDate, endDate) => {
+const getDatesBetween = (startDate: string | number | Date, endDate: number | Date) => {
     let dates = [];
     let loop = new Date(startDate);
     while (loop <= endDate) {
@@ -30,8 +30,8 @@ var getDatesBetween = (startDate, endDate) => {
     return dates;
 }
 
-var debitableHoursPerDay = (startDate, endDate) => {
-    let debitableHoursPerDay = [];
+const debitableHoursPerDay = (startDate: Date, endDate: Date) => {
+    let debitableHoursPerDay: { Day: Date; Hours: number; Month: number; Year: number; }[] = [];
     var dates = getDatesBetween(startDate, endDate);
 
     dates.forEach(date => {
@@ -49,10 +49,10 @@ var debitableHoursPerDay = (startDate, endDate) => {
     return debitableHoursPerDay;
 };
 
-var maximumDebitableHoursGrouped = (debitableHoursPerDay, groupedBy) => {
+const maximumDebitableHoursGrouped = (debitableHoursPerDay: any[], groupedBy: string) => {
     var groupedByMonth = _(debitableHoursPerDay)
         .groupBy(groupedBy)
-        .map((item, timeFrame) => ({
+        .map((item: any, timeFrame: any) => ({
             timeFrame: timeFrame,
             totalHours: _.sumBy(item, "Hours")
         }))
@@ -61,27 +61,27 @@ var maximumDebitableHoursGrouped = (debitableHoursPerDay, groupedBy) => {
     return groupedByMonth;
 }
 
-let defaultHolidays = () => {
+const defaultHolidays = () => {
     let defaultHolidays = [];
     defaultHolidays.push(...getDatesBetween(new Date(2022, 5, 1), new Date(2022, 6, 16)));
     defaultHolidays.push(...getDatesBetween(new Date(2022, 11, 15), new Date(2022, 11, 31)));
     return defaultHolidays;
 };
 
-let defaultBenchTime = () => {
+const defaultBenchTime = () => {
     let defaultBenchTime = [];
     defaultBenchTime.push(...getDatesBetween(new Date(2022, 8, 1), new Date(2022, 8, 15)));
     return defaultBenchTime;
 };
 
-let defaultSickness = () => {
+const defaultSickness = () => {
     let defaultSickness = [];
     defaultSickness.push(...getDatesBetween(new Date(2022, 2, 1), new Date(2022, 2, 6)));
     defaultSickness.push(...getDatesBetween(new Date(2022, 9, 15), new Date(2022, 9, 22)));
     return defaultSickness;
 }
 
-let defaultEducation = () => {
+const defaultEducation = () => {
     let defaultEducation = [];
     defaultEducation.push(...getDatesBetween(new Date(2022, 1, 15), new Date(2022, 1, 17)));
     defaultEducation.push(...getDatesBetween(new Date(2022, 3, 15), new Date(2022, 3, 16)));
@@ -93,11 +93,11 @@ let defaultEducation = () => {
 
 let dhpd = debitableHoursPerDay(new Date(2022, 0, 1), new Date(2022, 11, 31));
 
-let containsDate = (arrayOfDates, item) => {
-    return arrayOfDates.some(date =>
+const containsDate = (arrayOfDates: any[], item: Date) => {
+    return arrayOfDates.some((date: Date) =>
         date.getDate() === item.getDate()
         && date.getMonth() === item.getMonth()
-        && date.getYear() === item.getYear())
+        && date.getFullYear() === item.getFullYear())
 };
 
 let debitableHoursPerDayRemoveNonWorkingDays = dhpd.filter((item) => {
@@ -114,23 +114,14 @@ let debitableHoursPerDayRemoveNonWorkingDays = dhpd.filter((item) => {
     }
 })
 
-var yearly = maximumDebitableHoursGrouped(debitableHoursPerDayRemoveNonWorkingDays, "Year");
-
-// how to calculate salary and show what employees get
-// list all costs/benefits
-// education (fixed)
-// office supplies & software licences (fixed)
-// salary (calculated based on houerly rate)
-// pension (calculated based on salary)
-// vacation pay (statement)
-// sick pay (statement)
+let yearly = maximumDebitableHoursGrouped(debitableHoursPerDayRemoveNonWorkingDays, "Year");
 
 let getFixedCosts = () => {
     let fixedCosts = [];
     fixedCosts.push(
         { name: "WellnessAllowance", amount: 5000 },
         { name: "ComputerAndPhone", amount: 30000 }, 
-        { name: "Education", amount: 20000 },
+        { name: "Education", amount: 30000 },
         { name: "Conferences", amount: 20000 },
         { name: "SoftwareAndLicences", amount: 10000 },
         { name: "Bookkeeping", amount: 5000 },
@@ -141,61 +132,77 @@ let getFixedCosts = () => {
     return fixedCosts;
 }
 
-let roundToThousands = (number) => {
+let roundToThousands = (number: number) => {
     return Math.round(number / 1000)*1000;
 };
 
-let deductFixedCosts = (number, fixedCosts) => {
+let deductFixedCosts = (number: number, fixedCosts: any) => {
     return number - _.sumBy(fixedCosts, 'amount');
 };
 
-let deductCompanyMargin = (number, companyMarginPercentage) => {
+let deductCompanyMargin = (number: number, companyMarginPercentage: number) => {
     return Math.floor(number * (1 - (companyMarginPercentage/100)) / 1); 
 };
 
-let deductTaxesAndPension = (number, employeerContributionPercentage, pensionPercentage) => {
+let deductTaxesAndPension = (number: number, employeerContributionPercentage: number, pensionPercentage: number) => {
     return number / (1+(employeerContributionPercentage/100)+(pensionPercentage/100));
 };
-let deductBonusPercentage = (number, bonusPercentage) => {
+let deductBonusPercentage = (number: number, bonusPercentage: number) => {
     return (number / ((1 + (bonusPercentage/100)) * 100)) * 100;
 };
 
-let getBaseSalary = (employeerContributionPercentage, pensionPercentage, rate, debitedHours, fixedCosts, companyMarginPercentage, bonusPercentage) => {
+let getBaseSalary = (employeerContributionPercentage: number, pensionPercentage: number, rate: number, debitedHours: any, companyMarginPercentage: number, bonusPercentage: number) => {
     let income = getTotalIncome(debitedHours, rate);
-    let incomeAfterFixedCosts = deductFixedCosts(income, fixedCosts)
-    let incomeAfterMarginPerMonth = deductCompanyMargin(incomeAfterFixedCosts, companyMarginPercentage) / 12;
+    let incomeAfterMarginPerMonth = deductCompanyMargin(income, companyMarginPercentage) / 12;
     let leftForSalary = deductTaxesAndPension(incomeAfterMarginPerMonth, employeerContributionPercentage, pensionPercentage);
     let salaryExcludingBonus = deductBonusPercentage(leftForSalary, bonusPercentage);
 
     return roundToThousands(salaryExcludingBonus)
 };
 
-let getSalaryIncludingBonus = (baseSalary, bonusPercentage) => {
+let getSalaryIncludingBonus = (baseSalary: number, bonusPercentage: number) => {
     let salaryInclBonus = baseSalary + (baseSalary * (bonusPercentage/100));
     return roundToThousands(salaryInclBonus);
 };
 
 let rolesAndRates = [
     { role: "straighFromSchool", rate: 600 },
-    { role: "junior", rate: 700 },
-    { role: "medium", rate: 800 },
-    { role: "senior", rate: 900 },
-    { role: "expert", rate: 1000 },
+    { role: "junior 1-2 years", rate: 700 },
+    { role: "medium 3-5 years", rate: 800 },
+    { role: "senior 6-8 years", rate: 900 },
+    { role: "expert 8+ years", rate: 1150 },
 ];
 
 const employerContributionPercentage = 31.42;
 const pensionPercentage = 7.5;
-const companyMarginPercentage = 12;
+const companyMarginPercentage = 22;
 const bonusPercentage = 12;
 
 rolesAndRates.forEach((item) => {
-    let baseSalary = getBaseSalary(employerContributionPercentage, pensionPercentage, item.rate, yearly[0].totalHours, getFixedCosts(), companyMarginPercentage, bonusPercentage);
+    let baseSalary = getBaseSalary(employerContributionPercentage, pensionPercentage, item.rate, yearly[0].totalHours, companyMarginPercentage, bonusPercentage);
     let salaryInclBonus = getSalaryIncludingBonus(baseSalary, bonusPercentage);
     console.log("/////////////////////  Role: " + item.role + " /////////////////////////////");
     console.log("Rate: " + item.rate + "kr/h");
     console.log("Base Salary: " + baseSalary);
     console.log("Salary with bonus " + bonusPercentage + "% : " + salaryInclBonus);
-    console.log("pension: " + Math.floor(calculatePensionCostPerMonth(salaryInclBonus)/100)*100);
+    console.log("pension: " + calculatePensionCostPerMonth(salaryInclBonus));
     console.log("///////////////////////////////////////////////////////////////////////////");
     console.log("");
 });
+
+export const getInfo = () => {
+    const info: { baseSalary: number; salaryInclBonus: number; pension: number; }[] = [];
+    rolesAndRates.forEach((item) => {
+        let baseSalary = getBaseSalary(employerContributionPercentage, pensionPercentage, item.rate, yearly[0].totalHours, companyMarginPercentage, bonusPercentage);
+        let salaryInclBonus = getSalaryIncludingBonus(baseSalary, bonusPercentage);
+        let pension = calculatePensionCostPerMonth(salaryInclBonus);
+
+        info.push({
+            baseSalary,
+            salaryInclBonus,
+            pension
+        });
+    });
+
+    return info;
+}
