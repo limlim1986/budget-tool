080651cfeb43b3945c369e6d02bb26471912549d
Liam Salomonsson <liam.salomonsson@telenor.se>
2022-03-09 13:24:43 +0100

    selection of role work

--- shiftkey-offer/components/Selector.tsx
+++ shiftkey-offer/components/Selector.tsx
@@ -8,14 +8,22 @@ type SelectorProps = {
 }
 
 const Selector = ({ roles, selectedRole, setSelectedRole }: SelectorProps) => {
-  const setRole = (e: HTMLDivElement) => {
-    console.log(e.id);
+  const setRole = (e: HTMLButtonElement) => {
+    var role = roles.find(r => {
+      return r.roleName === e.id
+    });
+
+    setSelectedRole(role);
   }
 
   return (<div className='flex'>
     <div>{JSON.stringify(selectedRole)}</div>
     {roles.map((role, i) => (
-      <div onClick={({ currentTarget }) => setRole(currentTarget)} className='flex-1 border' key={role.roleName} id={role.roleName}>{role.roleName}</div>
+      <button onClick={({ currentTarget }) => setRole(currentTarget)}
+        className='flex-1 border'
+        key={role.roleName}
+        id={role.roleName}>{role.roleName}
+      </button>
     ))}
   </div>)
 }
