cb32a32c7b80bd6ecdc817c1085461b8ad3b8b3c
Liam Salomonsson <liam.salomonsson@telenor.se>
2022-03-29 15:09:15 +0200

    test husky

--- react-boilerplate/src/App.tsx
+++ react-boilerplate/src/App.tsx
@@ -3,11 +3,11 @@ import IMAGE from "./react.jpg"
 import { ClickCounter } from "./ClickCounter"
 
 export const App = () => {
-  const nema = 'testing';
+	const nema = "testing "
 	return (
 		<>
 			<h1>
-				React Boilerplate {JSON.stringify(process.env.NODE_ENV)} -{" "}
+				React Boilerplate {JSON.stringify(process.env.NODE_ENV)} -{nema}
 				{process.env.name}
 			</h1>
 			<img src={IMAGE} alt="React Logo" width={300} height={200} />
