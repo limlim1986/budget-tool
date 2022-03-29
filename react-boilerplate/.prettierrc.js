d1d3581efa642f51c69118e9997e64922915e69e
Liam Salomonsson <liam.salomonsson@telenor.se>
2022-03-29 14:45:59 +0200

    boiler plate work

---
+++ react-boilerplate/.prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  semi: false,
+  trailingComma: 'es5',
+  singleQuote: false,
+  jsxSingleQuote: false,
+  printWidth: 80,
+  useTabs: true,
+  tabWidth: 2,
+  endOfLine: 'auto'
+}
