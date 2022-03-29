c7f4c8a4841ce43e14665d79aea70aa7e59c4730
Liam Salomonsson <liam.salomonsson@telenor.se>
2022-03-29 14:46:51 +0200

    testing husky

---
+++ react-boilerplate/src/App.tsx
@@ -0,0 +1,17 @@
+import "./styles.css"
+import IMAGE from "./react.jpg"
+import { ClickCounter } from "./ClickCounter"
+
+export const App = () => {
+  const nema = "testing";
+	return (
+		<>
+			<h1>
+				React Boilerplate {JSON.stringify(process.env.NODE_ENV)} -{" "}
+				{process.env.name}
+			</h1>
+			<img src={IMAGE} alt="React Logo" width={300} height={200} />
+			<ClickCounter />
+		</>
+	)
+}
